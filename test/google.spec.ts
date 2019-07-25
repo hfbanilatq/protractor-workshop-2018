fe3a4acff9c42df3871800415f674fd624eb80a3
hfbanilatq <hfbanilatq@gmail.com>
2019-07-25 12:50:28 -0500

    Subiendo los cambios en google.spec.ts

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -2,11 +2,11 @@ import { browser } from 'protractor';
 
 describe('Given a SDET learning protracor', () => {
   describe('When open Google page', () => {
-    beforeEach(() => {
-      browser.get('http://www.google.com');
+    beforeEach(async () => {
+      await browser.get('http://www.google.com');
     });
-    it('then should have a title', () => {
-      expect(browser.getTitle()).toEqual('Google');
+    it('then should have a title', async () => {
+      await expect(browser.getTitle()).toEqual('Google');
     });
   });
 });
