28ab7e07ae62535ea0f8f748a70e5271e85a2e9f
hfbanilatq <hfbanilatq@gmail.com>
2019-07-26 17:26:01 -0500

    Instalando las dependencias para analisis de codigo estatico

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,8 +1,8 @@
-import { SpecReporter } from 'jasmine-spec-reporter'
-export let reporter= () => {
-    jasmine.getEnv().addReporter(new SpecReporter({
-        spec: {
-            displayStacktrace: true
-        }
-    }));
+import { SpecReporter } from 'jasmine-spec-reporter';
+export let reporter = () => {
+  jasmine.getEnv().addReporter(new SpecReporter({
+    spec: {
+      displayStacktrace: true
+    }
+  }));
 };
