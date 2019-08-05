0de122e8e1e9eb6c5b6bb02dd9c3f060b60b29d9
hfbanilatq <hfbanilatq@gmail.com>
2019-08-05 14:11:00 -0500

    agregando menucontent

---
+++ src/page/menu-content.page.ts
@@ -0,0 +1,10 @@
+import { $, ElementFinder, promise } from 'protractor';
+export class MenuContentPage{
+  private tShirtMenu: ElementFinder;
+  constructor() {
+    this.tShirtMenu = $('#block_top_menu > ul >li:nth-child(3) >a');
+  }
+  public async goToTShirtMenu (): Promise<void> {
+    await this.tShirtMenu.click();
+  }
+}
