9cf7815b419c947deec4dae97c7731940209666a
hfbanilatq <hfbanilatq@gmail.com>
2019-08-12 19:46:17 -0500

    corrigiendo google.spec.js

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -2,8 +2,10 @@ import { browser } from 'protractor';
 
 describe('Given a SDET learning protracor', () => {
   describe('When open Google page', () => {
-    it('then should have a title', async () => {
+    beforeEach(async () => {
       await browser.get('http://www.google.com');
+    });
+    it('then should have a title', async () => {
       await expect(browser.getTitle()).toEqual('Google');
     });
   });
